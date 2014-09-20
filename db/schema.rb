3d25689975e0aff0528dfb515e189b34ae929cc4
Sarah Barnekow <mellamopepe@gmail.com>
2014-09-19 22:59:21 -0700

    adding a user model, it's currently broken

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906202958) do
+ActiveRecord::Schema.define(version: 20140920053308) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
