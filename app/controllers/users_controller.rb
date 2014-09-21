86116b9cea125d0cc40c3f720da787a52dcf8f61
Sarah Barnekow <mellamopepe@gmail.com>
2014-09-20 19:39:49 -0700

    added users controller, model, view for new

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+	def new
+		
+	end
+
+	def create 
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "you've registered!"
+			redirect_to '/'
+		else flash[:alert] = "please try again"
+			redirect_to :back
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+end
