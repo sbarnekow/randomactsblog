0ddea407a413c8556124c0a5ff282431aae19acd
Sarah Barnekow <mellamopepe@gmail.com>
2014-09-22 22:05:26 -0700

    added admin privileges and removed links from layout

--- Gemfile
+++ Gemfile
@@ -19,6 +19,10 @@ group :assets do
 	gem 'neat'
 end
 
+group :development do
+  gem 'better_errors'
+end
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920053308) do
+ActiveRecord::Schema.define(version: 20140922031450) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140920053308) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin"
   end
 
 end
