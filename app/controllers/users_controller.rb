38c1f78fc784ea690bd2e6b3443096fdb809642d
Sarah Barnekow <mellamopepe@gmail.com>
2014-09-24 22:09:41 -0700

    retracting workaround

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!, :only => [:destroy]
+	before_action :authenticate_user!, :only => [:index, :edit, :update, :destroy]
 
 	def index
 		@user = User.all 
