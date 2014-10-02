95df4b9c045a7e998f5e4d90ecaee27d14b936de
Sarah Barnekow <mellamopepe@gmail.com>
2014-10-01 21:28:11 -0700

    added validation script to user.rb, figured out slight js error :D)

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,14 +1,17 @@
-// function validateEmail (){ 
-// 	judge.validate(document.getElementById('user_email'), {
-// 	valid: function(element, messages){
-// 		element.style.border = '1px solid green';
-// 		alert(messages.join(','))
-// 	},
-// 	invalid: function(element, messages) {
-// 		element.style.border = '1px solid red';
-// 		alert(messages.join(','))
-// 	}
-// });
-$('document').ready(function (){
-	$('form.new_user').submit(function(){ alert ('hello!') });
-})
+function validateEmail () { 
+	judge.validate(document.getElementById('user_email'), {
+		valid: function(element, messages){
+			element.style.border = '1px solid green';
+			console.log(messages.join(','))
+		},
+		invalid: function(element, messages) {
+			element.style.border = '1px solid red';
+			console.log(messages.join(','))
+		}
+	});
+};
+
+$(document).ready(function (){
+	// console.log('hi')
+	$('form.new_user').submit( validateEmail() )
+});
